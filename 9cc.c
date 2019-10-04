79a3197cfb0b09e44717c5d715473f54b1269559
ymt19 <hfytgc@gmail.com>
2019-10-04 17:46:17 +0900

    比較演算子導入

--- 9cc.c
+++ 9cc.c
@@ -68,8 +68,10 @@ bool consume(char *op) {
 
 //つぎのトークンが期待している記号のときは、トークンを１つ読み進める
 //それ以外の場合にはエラーを報告
-void expect(char op) {
-	if (token->kind != TK_RESERVED || token->str[0] != op)
+void expect(char *op) {
+	if (token->kind != TK_RESERVED ||
+		strlen(op) != token->len ||
+		memcmp(token->str, op, token->len))
 		error_at(token->str, "'%c'ではありません", op);
 	token = token->next;
 }
@@ -89,12 +91,19 @@ bool at_eof() {
 }
 
 // 新しいトークンを作成してcurに繋げる
-Token *new_token(TokenKind kind, Token *cur, char *str) {
+Token *new_token(TokenKind kind, Token *cur, char *str, int len) {
 	Token *tok = calloc(1, sizeof(Token));
 	tok->kind = kind;
 	tok->str = str;
+	tok->len = len;
 	cur->next = tok;
 	return tok;
+}
+
+//２つの文字列の比較
+//pの先頭の文字列とq全体の文字列の比較
+bool startswith(char *p, char *q) {
+	return memcmp(p, q, strlen(q)) == 0;
 }
 
 //入力文字列pをトークナイズしてそれを返す
@@ -112,23 +121,38 @@ Token *tokenize(){
 			continue;
 		}
 
-		if(strchr("+-()*/", *p)) {
-			cur = new_token(TK_RESERVED, cur, p++);
+		if (startswith(p, "==") || startswith(p, "!=") ||
+			startswith(p, "<=") || startswith(p, ">=")) {
+				cur = new_token(TK_RESERVED, cur, p, 2);
+				p += 2;
+				continue;
+			}
+		
+		if (strchr("+-*/()<>", *p)) {
+			cur = new_token(TK_RESERVED, cur, p, 1);
+			p++;
 			continue;
 		}
 
+		//数値の場合
 		if(isdigit(*p)) {
-			cur = new_token(TK_NUM, cur, p);
+			cur = new_token(TK_NUM, cur, p, 0);
+
+			char *q = p;
 			cur->val = strtol(p, &p, 10);
+			cur->len = p - q;
 			continue;
 		}
 
 		error_at(token->str, "invalid token");
 	}
 
-	new_token(TK_EOF, cur, p);
+	new_token(TK_EOF, cur, p, 0);
 	return head.next;
 }
+
+
+//パーサー
 
 //抽象構文木のノードの種類
 typedef enum {
@@ -136,6 +160,10 @@ typedef enum {
 	ND_SUB, // -
 	ND_MUL, // *
 	ND_DIV, // /
+	ND_EQ,  // ==
+	ND_NE,  // !=
+	ND_LT,  // <
+	ND_LE,  // <=
 	ND_NUM, // 整数
 } NodeKind;
 
@@ -166,18 +194,65 @@ Node *new_node_num(int val) {
 
 
 Node *expr();
+Node *equality();
+Node *relational();
+Node *add();
 Node *mul();
 Node *umary();
 Node *primary();
 
+//expr       = equality
 Node *expr() {
+	return equality();
+}
+
+//equality   = relational ("==" relational | "!=" relational)*
+Node *equality() {
+	Node *node = relational();
+
+	for(;;) {
+		if(consume("==")) {
+			node = new_node(ND_EQ, node, relational());
+		}
+		else if(consume("!=")) {
+			node = new_node(ND_NE, node, relational());
+		}
+		else
+			return node;
+	}
+}
+
+//relational = add ("<" add | "<=" add | ">" add | ">=" add)*
+Node *relational() {
+	Node *node = add();
+
+	for(;;) {
+		if(consume("<")) {
+			node = new_node(ND_LT, node, add());
+		}
+		else if(consume("<=")){
+			node = new_node(ND_LE, node, add());
+		}
+		else if(consume(">")) {
+			node = new_node(ND_LT, add(), node);
+		}
+		else if(consume(">=")) {
+			node = new_node(ND_LE, add(), node);
+		}
+		else
+			return  node;
+	}	
+}
+
+//add = mul ("+" mul | "-" mul)*
+Node *add() {
 	Node *node = mul();
 
 	for(;;) {
-		if(consume('+')) {
+		if(consume("+")) {
 			node = new_node(ND_ADD, node, mul());
 		}
-		else if(consume('-')) {
+		else if(consume("-")) {
 			node = new_node(ND_SUB, node, mul());
 		}
 		else
@@ -185,14 +260,15 @@ Node *expr() {
 	}
 }
 
+//mul = unary ("*" unary | "/" unary)*
 Node *mul() {
 	Node *node = umary();
 
 	for(;;) {
-		if(consume('*')) {
+		if(consume("*")) {
 			node = new_node(ND_MUL, node, umary());
 		}
-		else if(consume('/')) {
+		else if(consume("/")) {
 			node = new_node(ND_DIV, node, umary());
 		}
 		else
@@ -200,19 +276,21 @@ Node *mul() {
 	}
 }
 
+//unary = ("+" | "-")? primary
 Node *umary() {
-	if (consume('+'))
+	if (consume("+"))
 		return primary();
-	if (consume('-'))
+	if (consume("-"))
 		return new_node(ND_SUB, new_node_num(0), primary());
 	return primary();
 }
 
+//primary = num | "(" expr ")"
 Node *primary() {
 	//次のトークンが'('なら、'(' expr ')' のはず
-	if(consume('(')) {
+	if(consume("(")) {
 		Node *node = expr();
-		expect(')');
+		expect(")");
 		return node;
 	}
 
@@ -246,6 +324,26 @@ void gen(Node *node) {
 	case ND_DIV:
 		printf("	cqo\n");
 		printf("	idiv rdi\n");
+		break;
+	case ND_EQ:
+		printf("	cmp rax, rdi\n");
+		printf("	sete al\n");
+		printf("	movzb rax, al\n");
+		break;
+	case ND_NE:
+		printf("	cmp rax, rdi\n");
+		printf("	setne al\n");
+		printf("	movzb rax, al\n");
+		break;
+	case ND_LT:
+		printf("	cmp rax, rdi\n");
+		printf("	setl al\n");
+		printf("	movzb rax, al\n");
+		break;
+	case ND_LE:
+		printf("	cmp rax, rdi\n");
+		printf("	setle al\n");
+		printf("	movzb rax, al\n");
 		break;
 	}
 
